3f406656df83afa95be38b83417812a5e3fa4c03
osfor <panoramis@yandex.ru>
2025-04-10 10:51:08 +0300

    изменил

--- Program.cs
+++ Program.cs
@@ -4,12 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-//reads docker run args and overrides settings
-builder.Configuration
-    .AddJsonFile("appsettings.json")
-    .AddCommandLine(args);
-    //.AddEnvironmentVariables("TestApp_");
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
